9822bf7299e8214d9e7dc35fe4c45f3283864898
vernetto <abhinavhavaldar@hotmail.com>
2015-03-17 03:06:46 -0400

    thank god

--- main.js
+++ main.js
@@ -42,30 +42,6 @@ $(document).ready(function() {
 					$('#ChampRange').html(data.stats.attackrange);
 					$('#ChampMovementSpeed').html(data.stats.movespeed);	
 				});
-
-				$(".pop").hide();
-				$('#ChampName').html(name);
-				var path = 'Images/champion/' + $('#ChampName').html() + '.png';
-				$('#ChampImage').attr('src', path);
-				$('#ChampHPRegen').html(data.stats.hpregen);
-				$('#ChampManaRegen').html(data.stats.mpregen);
-				$('#ChampHealth').html(data.stats.hp);
-				$('#ChampMana').html(data.stats.mp);
-				$('#ChampArmorPenetration').html('0 | 0%');
-				$('#ChampMagicPenetration').html('0 | 0%');
-				$('#ChampCriticalDamage').html(200);
-				$('#ChampCriticalChance').html(0);
-				$('#ChampSpellVamp').html(0);
-				$('#ChampLifeSteal').html(0);
-				$('#ChampCooldownReduction').html(0);
-				$('#ChampAbilityPower').html(0);
-				$('#ChampAttackDamage').html(0);
-				$('#ChampAttackSpeed').html(0);
-				$('#ChampArmor').html(data.stats.armor);
-				$('#ChampMagicResist').html(data.stats.spellblock);
-				$('#ChampRange').html(data.stats.attackrange);
-				$('#ChampMovementSpeed').html(data.stats.movespeed);	});
-
 			}); 
 		});
 	});
@@ -108,31 +84,7 @@ $(document).ready(function() {
 					$('#OppRange').html(data.stats.attackrange);
 					$('#OppMovementSpeed').html(data.stats.movespeed);	
 				});
-
-				$(".pop").hide();
-				$('#OppName').html(name);
-				var path = 'Images/champion/' + $('#OppName').html() + '.png';
-				$('#OppImage').attr('src', path);
-				$('#OppHPRegen').html(data.stats.hpregen);
-				$('#OppManaRegen').html(data.stats.mpregen);
-				$('#OppMana').html(data.stats.mp);
-				$('#OppHealth').html(data.stats.hp);
-				$('#OppArmorPenetration').html('0 | 0%');
-				$('#OppMagicPenetration').html('0 | 0%');
-				$('#OppCriticalDamage').html(200);
-				$('#OppCriticalChance').html(0);
-				$('#OppSpellVamp').html(0);
-				$('#OppLifeSteal').html(0);
-				$('#OppCooldownReduction').html(0);
-				$('#OppAbilityPower').html(0);
-				$('#OppAttackDamage').html(0);
-				$('#OppAttackSpeed').html(0);
-				$('#OppArmor').html(data.stats.armor);
-				$('#OppMagicResist').html(data.stats.spellblock);
-				$('#OppRange').html(data.stats.attackrange);
-				$('#OppMovementSpeed').html(data.stats.movespeed);	
 			});
-			}); 
 		});
 	});
 	$('#boxclose').click(function() {
