addbc5e65d8cd5a93f84c48fdca2e7e405013fb7
Havaldar <abhinavhavaldar@hotmail.com>
2015-07-28 20:11:50 +0530

    made changes to tables and added profile pics

--- app/assets/javascripts/champions.js
+++ app/assets/javascripts/champions.js
@@ -3,7 +3,6 @@ angular.module('Champion', []).controller('ChampionCtrl', function($scope, $http
 	var api_version = "1.2";
 	var region = "na";
 	var url = "https://global.api.pvp.net/api/lol/static-data/"+region+"/v"+api_version+"/champion?champData=all&api_key="+key;
-	var array = [];
 	var dd_version = "5.14.1";
 	$http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key=40c0236b-48f3-4f53-93e5-4966062c87e7").success(function(data){
 		dd_version = data[0];
@@ -12,11 +11,9 @@ angular.module('Champion', []).controller('ChampionCtrl', function($scope, $http
 	$http.get(url).success(function(data){
 		for (var i in data["data"]) {
 			data["data"][i]["image_url"] = "http://ddragon.leagueoflegends.com/cdn/"+dd_version+"/img/champion/" + data["data"][i]["image"]["full"];
-			data["data"][i]["prfile_image_url"] = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/"+i+"_"+data["data"][i]["skins"][0]["num"] 
-			+ ".jpg";
-			array.push(data["data"][i]);
+			data["data"][i]["profile_image_url"] = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/"+i+"_"+data["data"][i]["skins"][0]["num"]+".jpg";
+			$scope.champions.push(data["data"][i]);
 		};	
-		$scope.champions = array;
 	});
 	$scope.attackChampion = null;
 	$scope.setAttackChampion = function(champion) {
@@ -25,5 +22,16 @@ angular.module('Champion', []).controller('ChampionCtrl', function($scope, $http
 	$scope.defendChampion = null;
 	$scope.setDefendChampion = function(champion) {
 		$scope.defendChampion = champion;
+	};
+
+	$scope.tagFilter = function (champion) {
+		for (var i in $scope.checked_tags) {
+			for (var j in champion.tags) {
+				if (i === j) {
+					return true;
+				}
+			}	
+		}
+    	return false;
 	};
 });
